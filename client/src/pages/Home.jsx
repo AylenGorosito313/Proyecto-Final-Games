c43502c8d895c8aadb577617eb59dfebda56b79e
Daniel9822 <dhdany64@gmail.com>
2022-12-14 16:27:45 -0400

    swiper-js
    carrusel con swiper.js

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,34 +5,44 @@ import Card from "../components/Cards";
 import "./Style-pages/Home.css";
 import img from "../assets/backg.png";
 import details from "../assets/details1.png";
+import SwiperPage from "../components/swiper-pagination/swiper";
 function Home() {
-  const dispatch = useDispatch();
-  const { users, games } = useSelector((state) => state.prueba);
+    const dispatch = useDispatch();
+    const { users, games } = useSelector((state) => state.prueba);
 
-  console.log(games);
-  useEffect(() => {
-    dispatch(getGames());
-  }, [dispatch]);
-  return (
-    <div className="div-home">
-      
-
-      <div className="div-home-card">
-      <img className="img-background" src={details} alt="background" />
-      <img className="img-background" src={img} alt="background" />
-        {games.length > 0 &&
-          games.map((ele) => {
-            return (
-              <Card
-                key={ele.name}
-                img={ele.background_image}
-                name={ele.name}
-                id={ele.id}
-              />
-            );
-          })}
-      </div>
-    </div>
-  );
+    console.log(games);
+    useEffect(() => {
+        dispatch(getGames());
+    }, [dispatch]);
+    return (
+        <>
+            <SwiperPage />
+            <div className="div-home">
+                <div className="div-home-card">
+                    <img
+                        className="img-background"
+                        src={details}
+                        alt="background"
+                    />
+                    <img
+                        className="img-background"
+                        src={img}
+                        alt="background"
+                    />
+                    {games.length > 0 &&
+                        games.map((ele) => {
+                            return (
+                                <Card
+                                    key={ele.name}
+                                    img={ele.background_image}
+                                    name={ele.name}
+                                    id={ele.id}
+                                />
+                            );
+                        })}
+                </div>
+            </div>
+        </>
+    );
 }
 export default Home;

bbead1c544be1f0025440734db3da049747e844f
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2022-12-21 10:51:54 -0300

    a

--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -1,15 +1,44 @@
-import axios from "axios"
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { getAllGames } from "../reducers/prueba/pruebaSlider";
 
-import {getAllGames} from "../reducers/prueba/pruebaSlider";
-
-function getGames (){
-    return async function (dispatch) {
-        let res = await axios({
-          method: "GET",
-          url: `http://localhost:3001/`,
-        });
-        dispatch(getAllGames(res.data));
+function getGames() {
+  return async function (dispatch) {
+    try {
+      let res = await axios({
+        method: "GET",
+        url: `http://localhost:3001/`,
+      });
+      dispatch(getAllGames(res.data));
+    
+    } catch (error) {
+ 
+    }
+  };
 }
-}
 
-export default getGames
+export default getGames;
+
+// toast('Hello World', {
+//   duration: 4000,
+//   position: 'top-center',
+
+//   // Styling
+//   style: {},
+//   className: '',
+
+//   // Custom Icon
+//   icon: '👏',
+
+//   // Change colors of success/error/loading icon
+//   iconTheme: {
+//     primary: '#000',
+//     secondary: '#fff',
+//   },
+
+//   // Aria
+//   ariaProps: {
+//     role: 'status',
+//     'aria-live': 'polite',
+//   },
+// });
--- client/src/reducers/prueba/pruebaSlider.js
+++ client/src/reducers/prueba/pruebaSlider.js
@@ -1,26 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    users: [],
-    games: []
+  users: [],
+  games: [],
 };
 
-
-
 export const toolkit_prueba = createSlice({
-    name: "redux-toolkit",
-    initialState,
-    reducers: {
-        addUser: (state, actions) => {
-            state.users = [...actions.payload]
-        },
-        getAllGames: (state, actions) => {
-            state.games= [...actions.payload]
-        }
+  name: "redux-toolkit",
+  initialState,
+  reducers: {
+    addUser: (state, actions) => {
+      state.users = [...actions.payload];
     },
+    getAllGames: (state, actions) => {
+      state.games = [...actions.payload];
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const {addUser, getAllGames} = toolkit_prueba.actions;
+export const { addUser, getAllGames } = toolkit_prueba.actions;
 
 export default toolkit_prueba.reducer;

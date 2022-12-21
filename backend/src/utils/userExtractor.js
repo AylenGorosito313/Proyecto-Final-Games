fa06485aae8e2c1383d90a92c0d655b9761c67a4
Daniel9822 <dhdany64@gmail.com>
2022-12-21 18:09:57 -0400

    updated userExtractor
    funcion que se encarga de verificar la autenticacion del usuario

--- backend/src/utils/userExtractor.js
+++ backend/src/utils/userExtractor.js
@@ -3,16 +3,20 @@ const { SECRET } = process.env;
 
 module.exports = (req, res, next) => {
     try {
+        //authorizacion la optenemos del request headers
         let authorizacion = req.get("Authorization");
         let token = "";
 
+        //verificamos si no nos mandaron el header Authorizacion
         if (!authorizacion)
             return res.status(401).json({ error: "token must be provided" });
 
+        //si tenemos authorizacion verificamos que tengamos la parte de bearer y tomamos solo la parte del token 
         if (authorizacion && authorizacion.toLowerCase().startsWith("bearer")) {
             token = authorizacion.substring(7);
         }
 
+        //decodificamos el token con la palabra secreta
         const decodedToken = jwt.verify(token, SECRET);
 
         if (!decodedToken || !decodedToken.id) {

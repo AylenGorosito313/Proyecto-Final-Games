bf2ac05fb1ff51d4542369b5185d7656f428d4b9
Daniel9822 <dhdany64@gmail.com>
2022-12-21 21:55:20 -0400

    -redux-state-isloader

--- client/src/components/Nav/Nav-top.jsx
+++ client/src/components/Nav/Nav-top.jsx
@@ -8,36 +8,33 @@ import Car from "../../svg/Car";
 import Search from "../Search/Search";
 import SearchIcon from "../../svg/SearchIcon";
 function NavTop() {
-  const [open, setOpen] = useState(false);
-  function ToggleSearch() {
-    setOpen(!open);
-  }
-  return (
-    <div className="Nav-top-container">
-      <div className="div-logo">
-        <Logo />
-      </div>
+    const [open, setOpen] = useState(false);
+    function ToggleSearch() {
+        setOpen(!open);
+    }
+    return (
+        <div className="Nav-top-container">
+            <div className="div-logo">
+                <Logo />
+            </div>
 
-   
+            <div className="Nav-top-container-sticky">
+                {open && <Search />}
+                <div className="Svg">
+                    <div onClick={ToggleSearch} className="Layout-search-icon">
+                        <SearchIcon className="SearchIcon" />
+                    </div>
+                </div>
 
-      <div className="Nav-top-container-sticky">
-      {open && <Search />}
-      <div className="Svg">
-       
-          <div   onClick={ToggleSearch} className="Layout-search-icon">
-            <SearchIcon className="SearchIcon" />
-          </div>
+                <div className="div-carrito-notifi">
+                    <Car />
+                </div>
+                <div className="div-carrito-notifi">
+                    <Notificacion />
+                </div>
+            </div>
         </div>
-
-        <div className="div-carrito-notifi">
-          <Car />
-        </div>
-        <div className="div-carrito-notifi">
-          <Notificacion />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default NavTop;
--- client/src/reducers/prueba/pruebaSlider.js
+++ client/src/reducers/prueba/pruebaSlider.js
@@ -1,27 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  users: [],
-  games: [],
+    users: [],
+    games: [],
+    isLoader: false,
 };
 
 export const toolkit_prueba = createSlice({
-  name: "redux-toolkit",
-  initialState,
-  reducers: {
-    addUser: (state, actions) => {
-      state.users = [...actions.payload];
+    name: "redux-toolkit",
+    initialState,
+    reducers: {
+        addUser: (state, actions) => {
+            state.users = [...actions.payload];
+        },
+        getAllGames: (state, actions) => {
+            state.games = [...actions.payload];
+        },
+        getByName: (state, actions) => {
+            state.games = [...actions.payload];
+        },
+        setIloader: (state) => {
+            state.isLoader = !state.isLoader;
+        },
     },
-    getAllGames: (state, actions) => {
-      state.games = [...actions.payload];
-    },
-    getByName: (state, actions) => {
-      state.games= [...actions.payload]
- }
-  },
 });
 
 // Action creators are generated for each case reducer function
-export const { addUser, getAllGames, getByName } = toolkit_prueba.actions;
+export const { addUser, getAllGames, getByName, setIloader } =
+    toolkit_prueba.actions;
 
 export default toolkit_prueba.reducer;

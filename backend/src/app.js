a675ab0ac8dee36deeb376ee8a93ab38280c3888
Daniel9822 <dhdany64@gmail.com>
2022-12-27 17:15:30 -0400

    -Delete-models

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ const genreRouter = require('./routes/genres.routes')
 const gamesRouter = require('./routes/games.routes')
 const usersRouter = require('./routes/users.routes')
 const loginRouter = require('./routes/login.routes')
-const packsRouter = require('./routes/getPacks.routes')
 const cors = require('cors')
 const server = express()
 server.use(cors())
@@ -15,7 +14,7 @@ server.use(gamesRouter)
 server.use(loginRouter)
 server.use(genreRouter)
 server.use(usersRouter)
-server.use(packsRouter)
+
 
 
 

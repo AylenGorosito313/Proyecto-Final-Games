8cce9ffc11d3ce0ff96a503648f9e85ece830e23
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2022-12-27 21:23:52 -0300

    homer

--- client/src/components/Select/SelectProfile.jsx
+++ client/src/components/Select/SelectProfile.jsx
@@ -1,6 +1,10 @@
 import { useDispatch } from "react-redux";
 import "./SelectProfile.css";
+import Logout from "../../svg/icons-menu/logout";
+import Settings from "../../svg/icons-menu/settings";
+import Profile from "../../svg/icons-menu/profile";
 import { clearState } from "../../reducers/prueba/pruebaSlider";
+
 export default function SelectProfile({ setOpen, setLogin }) {
   const dispatch =useDispatch()
   const handlerLogout = () => {
@@ -15,14 +19,14 @@ export default function SelectProfile({ setOpen, setLogin }) {
     <div className="select-layout">
    <div className="select-container">
       <div className="option-container">
-        <p className="option-name">Profile</p>
+      <Profile/>  <p className="option-name">  Profile</p>
       </div>
       <div className="option-container">
-        <p className="option-name">Settings</p>
+       < Settings/> <p className="option-name">Settings</p>
       </div>
 
       <div className="option-container" onClick={handlerLogout}>
-        <p className="option-name">Logut</p>
+      <Logout className="div-icon-menu-perfil" />   <p className="option-name">Logut</p>
       </div>
     </div>
     </div>
---
+++ client/src/svg/icons-menu/Logout.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Logout() {
+    return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="17"
+          fill="none"
+          viewBox="0 0 20 17"
+        >
+          <path
+            fill="#F9F9F9"
+            fillRule="evenodd"
+            d="M12 9H0V8h12v1z"
+            clipRule="evenodd"
+          ></path>
+          <path
+            fill="#F9F9F9"
+            fillRule="evenodd"
+            d="M8.854 4.646l3.5 3.5a.5.5 0 010 .708l-3.5 3.5-.708-.707L11.293 8.5 8.146 5.354l.708-.708z"
+            clipRule="evenodd"
+          ></path>
+          <path
+            fill="#F9F9F9"
+            fillRule="evenodd"
+            d="M11 1a7.5 7.5 0 00-6.68 4.088l-.891-.456a8.5 8.5 0 110 7.736l.89-.456A7.5 7.5 0 1011 1z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      );
+}

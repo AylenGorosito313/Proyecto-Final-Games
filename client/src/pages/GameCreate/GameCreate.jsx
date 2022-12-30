0aecfa7fd91addb5162abcf729c74ca4160e0489
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2022-12-29 22:46:38 -0300

    Select / genero
    solucion a guardar los generos en un array

--- client/src/pages/GameCreate/GameCreate.jsx
+++ client/src/pages/GameCreate/GameCreate.jsx
@@ -1,77 +1,102 @@
-import React from 'react'
-import { useEffect} from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import {useHistory} from "react-router-dom";
-import { useDispatch,useSelector } from "react-redux";
-import { CreateGame, traerGenero } from '../../middleware'
-import style from '../GameCreate/GameCreate.module.css'
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { CreateGame, traerGenero } from "../../middleware";
+import style from "../GameCreate/GameCreate.module.css";
 
 export default function GameCreate() {
-    const dispatch = useDispatch();
-   //  const history = useHistory();
-    const {genre}= useSelector((state) => state.prueba)
+  const dispatch = useDispatch();
+  //  const history = useHistory();
+  const { genre } = useSelector((state) => state.prueba);
+  const [gender, setGender] = useState({
+    genere:[],
+  });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      name: "",
+      background_image: "",
+      rating: 0,
+      platforms: [],
+    },
+    mode: "onChange",
+  });
 
-    const {register, formState: { errors }, handleSubmit} = useForm({
-      defaultValues: {
-         name: "",
-         background_image: "",
-         rating: 0,
-         genre: [],
-         platforms: [],
-      },
-      mode: "onChange",
-    });
+  const onSubmit = async (data) => {
+    let genre = gender.genere
 
-    const onSubmit = async (data) => {
-        console.log(data)
-        dispatch(CreateGame(data));
-      //   history.push('/')
-    }
+    console.log({...data, genre});
+    dispatch(CreateGame(data));
+    //   history.push('/')
+  };
+
+  const handlerGender = (event) => {
+
+    setGender({...gender, genere:[...gender.genere, event.target.value] });
    
-    useEffect(() => {
-        dispatch(traerGenero());
-    }, []);
-   
+ 
+  };
+  console.log(gender);
+  useEffect(() => {
+    dispatch(traerGenero());
+  }, []);
 
   return (
     <div className={style.fondo}>
-       
-        <form onSubmit={handleSubmit(onSubmit)}>
-         <div>
-            <label> Name:</label>
-            <input type='text' placeholder='Name of the game' {...register('name', {
-                required: true,
-                minLength: 5,
-                maxLength: 20,
-            })} />
-         </div>
-         {errors.name?.type === 'required' && <p>The name is required</p>}
-         {errors.name?.type === 'maxLength' && <p>The name is too long</p>}
-         {errors.name?.type === 'minLength' && <p>The name is too short</p>}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label> Name:</label>
+          <input
+            type="text"
+            placeholder="Name of the game"
+            {...register("name", {
+              required: true,
+              minLength: 5,
+              maxLength: 20,
+            })}
+          />
+        </div>
+        {errors.name?.type === "required" && <p>The name is required</p>}
+        {errors.name?.type === "maxLength" && <p>The name is too long</p>}
+        {errors.name?.type === "minLength" && <p>The name is too short</p>}
 
-         <div>
-            <label> Image:</label>
-            <input type='text'  placeholder='Example http://...' {...register('background_image', {
-                pattern: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/
-            })} />
-         </div>
-         {errors.background_image?.type === 'pattern' && <p>The image must be a URL</p>}
+        <div>
+          <label> Image:</label>
+          <input
+            type="text"
+            placeholder="Example http://..."
+            {...register("background_image", {
+              pattern: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/,
+            })}
+          />
+        </div>
+        {errors.background_image?.type === "pattern" && (
+          <p>The image must be a URL</p>
+        )}
 
+        <div>
+          <label> Genre:</label>
+          <select onChange={handlerGender}>
+            {genre &&
+              genre.map((g, i) => (
+                <option value={g} key={i}>
+                  {g}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          <label> Platforms:</label>
+          <input type="text" {...register("platforms")} />
+        </div>
 
-         <div>
-            <label> Genre:</label>
-            <select {...register('genre')}>
-            {genre && genre.map((g,i) => <option key={i}>{g}</option>)}
-            </select>
-         </div>
-
-         <div>
-            <label> Platforms:</label>
-            <input type='text' {...register('platforms')} />
-         </div>
-
-          <button type="submit">Create Game</button>
-        </form>
+        <button type="submit">Create Game</button>
+      </form>
     </div>
-  )
+  );
 }

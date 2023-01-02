3fcbf81aba90a940f087aefd9092667e78fceee6
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-02 16:11:11 -0300

    Estilos nav-bar

--- client/src/svg/Notificacion.jsx
+++ client/src/svg/Notificacion.jsx
@@ -4,8 +4,8 @@ function notificacion() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="28"
-      height="28"
+      width="25"
+      height="25"
       fill="none"
       viewBox="0 0 24 24"
     >

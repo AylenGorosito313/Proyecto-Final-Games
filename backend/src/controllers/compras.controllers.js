e05f7ecfcf806c3587bd3040e1f3e649f4f22a41
Daniel9822 <dhdany64@gmail.com>
2023-01-05 23:27:27 -0400

    update

--- backend/src/controllers/compras.controllers.js
+++ backend/src/controllers/compras.controllers.js
@@ -5,7 +5,7 @@ const userPurchase = (req, res) => {
 
     try {
         let result = addPurchaseUser(userId)
-        res.status(200).json(result)
+        res.status(200).json({result})
     } catch (error) {
         res.status(400).json({
             error: error.message

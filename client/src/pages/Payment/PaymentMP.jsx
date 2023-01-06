e476bfe732807f21de2c483ed8c19e4faa1a62f5
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-06 11:14:01 -0300

    scroll carrtio

--- client/src/pages/Payment/PaymentMP.jsx
+++ client/src/pages/Payment/PaymentMP.jsx
@@ -6,39 +6,38 @@ import Cart from "../../components/Cart/Cart";
 import { getCart } from "../../middleware/index";
 import { clearStateCart } from "../../reducers/prueba/pruebaSlider";
 export default function PaymentMP() {
-  const { payment} = useSelector((state) => state.prueba);
- 
- 
+  const { payment } = useSelector((state) => state.prueba);
+
   const dispatch = useDispatch();
   useEffect(() => {
     let userId = localStorage.getItem("id");
     dispatch(getCart(userId));
-    return () => {dispatch(clearStateCart())}
+    return () => {
+      dispatch(clearStateCart());
+    };
   }, []);
 
   return (
     <>
       <div className="container-center-payment">
         <div className="container-payment">
-     
           <div className="cart-products-div">
-            
             <Cart />
-         
           </div>
-
-          <div className="payment-forme">
-
-          <a href={payment.link.data}>
-              <button className="button-3" role="button">
-                Comprar
-              </button>
-            </a>
-
+<div className="buttons-payments">
+ <a href={payment.link.data}>
             <button className="button-3" role="button">
-              Comprar para regalo
+              Comprar
             </button>
-          </div>
+          </a>
+
+          <button className="button-3" role="button">
+            Comprar para regalo
+          </button>
+
+
+</div>
+         
         </div>
       </div>
     </>

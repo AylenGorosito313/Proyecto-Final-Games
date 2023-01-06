535353ba0a9252e7111d2ab57776e8687185cdcf
Daniel9822 <dhdany64@gmail.com>
2023-01-05 20:59:44 -0400

    -payment-service

--- backend/src/controllers/Payment.controller.js
+++ backend/src/controllers/Payment.controller.js
@@ -1,34 +1,34 @@
-const {createPayment , createSubscription} = require("../services/PaymentService")
-
-
-
+const {
+    createPayment,
+    createSubscription,
+} = require("../services/PaymentService");
+const getItemsCar = require("../utils/getItemsCar");
 
 const getPaymentLink = async (req, res) => {
-  const games = req.body;
-  try {
-    const payment = await createPayment(games);
-    return res.json(payment.init_point);
-  } catch (error) {
-    console.log(error);
-
-    return res
-      .status(500)
-      .json({ error: true, msg: "Failed to create payment" });
-  }
+    const { id } = req.query;
+    try {
+        let carItems = await getItemsCar(id);
+        const payment = await createPayment(carItems);
+        return res.json(payment.init_point);
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ error: true, msg: "Failed to create payment" });
+    }
 };
 
 const getSubscriptionLink = async (req, res) => {
-  try {
-    const subscription = createSubscription();
+    try {
+        const subscription = createSubscription();
 
-    return res.json(subscription);
-  } catch (error) { 
-    console.log(error);
+        return res.json(subscription);
+    } catch (error) {
+        console.log(error);
 
-    return res
-      .status(500)
-      .json({ error: true, msg: "Failed to create subscription" });
-  }
+        return res
+            .status(500)
+            .json({ error: true, msg: "Failed to create subscription" });
+    }
 };
 
-module.exports = { getPaymentLink , getSubscriptionLink};
+module.exports = { getPaymentLink, getSubscriptionLink };

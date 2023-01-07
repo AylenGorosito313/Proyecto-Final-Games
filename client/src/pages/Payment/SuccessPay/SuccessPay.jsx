86c20fbad03dbd67353ba84a2b934ffba8286a93
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-07 00:30:03 -0300

    sucess payment

--- client/src/pages/Payment/SuccessPay/SuccessPay.jsx
+++ client/src/pages/Payment/SuccessPay/SuccessPay.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import "./Success.css"
- function SuccessPay() {
+import React from "react";
+import "./Success.css";
+import fondo from "../../../assets/img/payment.jpg";
+import LogoGrande from "../../../svg/Logos/LogoGrande";
+function SuccessPay() {
   return (
-    <div className='container-pay-succss'>
-         <div  className='container-succes-pay'>SuccessPay</div>
+    <div className="container-pay-succss">
+      <div className="fondo-scss-payment">
+        <img className="img-scss-payment" src={fondo} alt="fondo" />
+      </div>
+
+      <div className="container-succes-pay">
+        <div className="logo-scss-div">
+          <LogoGrande />
+        </div>
+        <div className="text-scss-div">
+          <p className="p-scss">
+            Thank you very much for making your purchase on Andromeda's game.
+            Your download link is now available and we hope you enjoy your game.
+            If you have any questions or need assistance, please do not hesitate
+            to contact us.
+          </p>
+        </div>
+        <div className="buttons-payment-sucss">
+        <button class="button-29" role="button">Download</button>
+        <button class="button-30" role="button">Ver mi perfil</button>
+        </div>
+     
+       
+      </div>
     </div>
-   
-  )
+  );
 }
 
-
-export default SuccessPay
+export default SuccessPay;

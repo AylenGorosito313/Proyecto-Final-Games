2de974fd3584b5177db583bc0f4b987ad5d021ee
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-12 15:58:05 -0300

    Foorter (sin funcionalidad)

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import style from "./utils/Footer.module.css";
 import Logo from "./utils/LogoFooter";
+import Facebook from "../../svg/Icons-SocialMedia/Icons-footer/Footer-Face";
+import GitHub from "../../svg/Icons-SocialMedia/Icons-footer/Footer-Git";
+import Linke from "../../svg/Icons-SocialMedia/Icons-footer/Footer-Ins";
 export default function Footer() {
   return (
     <div className={style.container}>
@@ -9,23 +12,35 @@ export default function Footer() {
 
         <div className={style.about}>
           <h1 className={style.h1}>About us</h1>
-          <p className={style.p}>Términos de servicio</p>
-          <p className={style.p}>Política de privacidad</p>
-          <p className={style.p}>Política de reembolso de la tienda</p>
+          <p className={style.p}>Development team</p>
+          <p className={style.p}>FAQs</p>
+          <p className={style.p}>Store Refund Policy</p>
         </div>
 
         <div className={style.contact}>
           <h1 className={style.h1}>Contact us </h1>
-          <p className={style.p}>Términos de servicio</p>
-          <p className={style.p}>Política de privacidad</p>
-          <p className={style.p}>Política de reembolso de la tienda</p>
+          <div className={style.socialDiv}>
+            <Facebook />
+            <p className={style.p}>Facebook</p>
+          </div>
+          <div className={style.socialDiv}> 
+          <GitHub/>
+          <p className={style.p}>GitHub</p>
+          </div>
+          <div className={style.socialDiv}> 
+          <Linke/>
+          <p className={style.p}>Linkedin</p>
+          </div>
+
+          
+         
         </div>
 
         <div className={style.resurces}>
           <h1 className={style.h1}>Resources</h1>
-          <p className={style.p}>Términos de servicio</p>
-          <p className={style.p}>Política de privacidad</p>
-          <p className={style.p}>Política de reembolso de la tienda</p>
+          <p className={style.p}>Blog</p>
+          <p className={style.p}>Learn more</p>
+          <p className={style.p}>Terms of Use</p>
         </div>
       </div>
       <div className={style.text}>

407d557a808c357aa5a306a3bee76ee63887917d
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-14 19:44:25 -0300

    css select

--- client/src/components/Select/SelectProfile.jsx
+++ client/src/components/Select/SelectProfile.jsx
@@ -36,30 +36,42 @@ export default function SelectProfile({ setOpen, setLogin }) {
     >
       <div className="select-container">
         <div className="option-container">
-          <Profile /> <p className="option-name"> Profile</p>
+          <div>
+            <Profile />
+          </div>
+          <p className="option-name"> Profile</p>
         </div>
         <div className="option-container">
-          <Settings /> <p className="option-name">Settings</p>
+          <div>
+            <Settings />
+          </div>
+          <p className="option-name">Settings</p>
         </div>
 
         {Idpovider ? (
           <div className="option-container">
-            <DeveloperIcon />{" "}
-            <Link to={"/game/form/create"}>
+            <div>
+              <DeveloperIcon />
+            </div>{" "}
+            <Link className="Link-select" to={"/game/form/create"}>
               <p className="option-name"> Unpload Game </p>
             </Link>
           </div>
         ) : (
           <div className="option-container">
-            <DeveloperIcon />{" "}
-            <Link to={"/proveedor"}>
+            <div>
+              <DeveloperIcon />
+            </div>{" "}
+            <Link className="Link-select" to={"/proveedor"}>
               <p className="option-name">Be Developer </p>
             </Link>
           </div>
         )}
 
         <div className="option-container" onClick={handlerLogout}>
-          <Logout className="div-icon-menu-perfil" />{" "}
+          <div>
+            <Logout className="div-icon-menu-perfil" />
+          </div>{" "}
           <p className="option-name">Logut</p>
         </div>
       </div>

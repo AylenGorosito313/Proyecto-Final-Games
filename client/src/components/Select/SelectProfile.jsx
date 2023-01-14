2f2f5f5944ec256e9715f0bfd7a90d2736b9523b
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-13 21:14:16 -0300

    detallistosx4

--- client/src/components/Select/SelectProfile.jsx
+++ client/src/components/Select/SelectProfile.jsx
@@ -1,10 +1,12 @@
 import { useDispatch } from "react-redux";
 import "./SelectProfile.css";
+import DeveloperIcon from "../../svg/icons-menu/Developer-Icon";
 import Logout from "../../svg/icons-menu/Logout";
 import Settings from "../../svg/icons-menu/Settings";
 import Profile from "../../svg/icons-menu/Profile";
 import { clearState } from "../../reducers/prueba/pruebaSlider";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 export default function SelectProfile({ setOpen, setLogin }) {
   const dispatch = useDispatch();
   const handlerLogout = () => {
@@ -32,7 +34,12 @@ export default function SelectProfile({ setOpen, setLogin }) {
         <div className="option-container">
           <Settings /> <p className="option-name">Settings</p>
         </div>
-
+        <div className="option-container">
+          <DeveloperIcon />{" "}
+          <Link to={'/proveedor'}>
+            <p className="option-name">Be Developer </p>
+          </Link>
+        </div>
         <div className="option-container" onClick={handlerLogout}>
           <Logout className="div-icon-menu-perfil" />{" "}
           <p className="option-name">Logut</p>

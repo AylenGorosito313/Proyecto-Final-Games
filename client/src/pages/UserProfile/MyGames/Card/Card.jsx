65cd1dd3d4962fb4054d17d3167f024eb141e3a0
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-15 10:59:03 -0300

    css

--- client/src/pages/UserProfile/MyGames/Card/Card.jsx
+++ client/src/pages/UserProfile/MyGames/Card/Card.jsx
@@ -4,31 +4,41 @@ import { Link } from "react-router-dom";
 import { platformImage } from "../../../CardDetail/utils/utils";
 
 export default function Card(props) {
-    return (
-     
-            <div className={style.conteiner}>
-                <div className={style.div}>
-                    <Link className={style.li} to={`/game/${props.id}`}>
-                        <img className={style.img} src={props.image}  alt={props.name} width="150px" height="90px" />
-                    </Link>
-                </div>
-                <div className={style.div}>{props.name}</div>
-                <div className={style.div}>
-                        {
-                        props.platforms ? props.platforms.slice(0,3).map( el => (
-                            <span key={el}> 
-                                {platformImage(el)} 
-                            </span>
-                        )) : 
-                        <span>No available for plataforms</span>}
-                </div>
-                <div className={style.div}>
-                    <i class="fa-solid fa-download"></i> 
-                </div>
-                <div >
-                  <button className={style.button}>🗑️</button>
-                </div>
-            </div>
-       
-    );
+  return (
+    <div className={style.conteiner}>
+      <div className={style.div}>
+        <Link  to={`/game/${props.id}`}>
+          <img
+            className={style.img}
+            src={props.image}
+            alt={props.name}
+            width="150px"
+            height="90px"
+          />
+        </Link>
+      </div>
+
+      <div className={style.div}>
+        <Link className={style.Link} to={`/game/${props.id}`}>
+          {props.name}
+        </Link>
+      </div>
+
+      <div className={style.div}>
+        {props.platforms ? (
+          props.platforms
+            .slice(0, 3)
+            .map((el) => <span key={el}>{platformImage(el)}</span>)
+        ) : (
+          <span>No available for plataforms</span>
+        )}
+      </div>
+      <div className={style.div}>
+        <i class="fa-solid fa-download"></i>
+      </div>
+      <div>
+        <button className={style.button}>🗑️</button>
+      </div>
+    </div>
+  );
 }

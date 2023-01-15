caba222bc3a6ff2369c2b1d8fd017d6f5e9f2656
Daniel9822 <dhdany64@gmail.com>
2023-01-15 04:24:20 -0400

    fix-login

---
+++ backend/src/controllers/createUserAndVerify.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Users } = require("../models/users");
+const { SECRET } = process.env;
+
+const createUser = async (user) => {
+    try {
+        const passwordHash = await bcrypt.hash(user.password, 10);
+        const createUser = await Users.create({
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            birth_date: user.birth_date,
+            profile_img: user.profile_img,
+            region: user.region,
+            passwordHash,
+        });
+        return createUser.toJSON();
+    } catch (error) {
+        return "oops something went wrong";
+    }
+};
+
+
+const verifyUser = (token) => {
+
+    try {
+       let result = jwt.verify(token, SECRET)
+       return true
+        
+    } catch (error) {
+        return false
+    }
+}
+
+module.exports = {
+    createUser,
+    verifyUser
+}

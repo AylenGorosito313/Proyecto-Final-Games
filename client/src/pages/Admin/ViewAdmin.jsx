a945d2c15d326989324816807196537f9db630c7
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-17 11:53:50 -0300

    a

--- client/src/pages/Admin/ViewAdmin.jsx
+++ client/src/pages/Admin/ViewAdmin.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import style from "./PanelAdmin.module.css";
-import LogoPanel from "../../svg/Logos/LogoPanel"
+
+import NavAdmin from "./NavAdmin/NavAdmin";
 import NavTop from "../../components/Nav/Nav-top";
 export default function ViewAdmin() {
   return (
     <>
      <div className={style.Layout}>
       <div className={style.Contairner}>
-        <div className={style.nav}>
-            <div className={style.Logo}>
-                    <LogoPanel/>
-            </div>
-        
-          
-        </div>
-       
+   
+       < NavAdmin/>
       </div>
     </div>
     

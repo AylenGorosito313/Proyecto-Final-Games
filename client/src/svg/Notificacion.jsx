fa44e91f12c791df20442e2e1c784be328fa6ded
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-17 14:24:58 -0300

    Navbar

--- client/src/svg/Notificacion.jsx
+++ client/src/svg/Notificacion.jsx
@@ -4,8 +4,8 @@ function notificacion() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="25"
-      height="25"
+      width="20"
+      height="20"
       fill="none"
       viewBox="0 0 24 24"
     >
--- client/src/svg/User.jsx
+++ client/src/svg/User.jsx
@@ -5,8 +5,8 @@ export default function User() {
         return (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
+              width="22"
+              height="22"
               fill="none"
               viewBox="0 0 20 20"
             >

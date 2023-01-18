10cc9bd611d1bf2939bd0afc6202835876f64ca1
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-18 18:55:25 -0300

    iconsNav

--- client/src/pages/Admin/NavAdmin/NavAdmin.jsx
+++ client/src/pages/Admin/NavAdmin/NavAdmin.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import style from "./NavAdmin.module.css";
 import LogoPanel from "../../../svg/Logos/LogoPanel";
 import { Link } from "react-router-dom";
+// import icons
+import BannerIcon from "../AdminSvg/BannersNav"
+import DashboardIcon from "../AdminSvg/DashboardNav"
+import GamesIcon from "../AdminSvg/GamesNav"
+import SettingsIcon from "../AdminSvg/SettingsNav"
+import UserIcon from "../AdminSvg/UserNav"
+
 export default function NavAdmin() {
   return (
     <div className={style.nav}>
@@ -11,20 +18,25 @@ export default function NavAdmin() {
 
       <div className={style.MetaContainer}>
       <div  className={style.container}>
+      <DashboardIcon />
         <p className={style.p}>Dashboard</p>
       </div>
    <div  className={style.container}>
+   <UserIcon  />
         <p className={style.p}>Admin. de Usuarios</p>
       </div>
       <div  className={style.container}>
+      <GamesIcon />
         <p className={style.p}>Admin. de Games</p>
       </div>
       <div  className={style.container}>
+      <BannerIcon/>
         <p className={style.p}>Admin. de Banners</p>
       </div>
 
 
       <div  className={style.container}>
+      <SettingsIcon />
         <p className={style.p}>Settings</p>
       </div>
 

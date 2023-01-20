b17b8a670da75fa83de00a0b4830be3b0263b2cd
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-19 21:39:25 -0300

    nav admin button

---
+++ client/src/svg/PanelAdminNav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function PanelAdminNav() {
+    return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="#fff"
+            strokeLinejoin="round"
+            d="M9 19.5H2v-16h17.5V9M19.5 8H2M5.5 2v3M15.5 2v3M15.086 16.414L17.5 14l1.5 1.5-2.414 2.414a2 2 0 01-1.414.586H14.5v-.672a2 2 0 01.586-1.414z"
+          ></path>
+          <circle
+            cx="16.5"
+            cy="16.5"
+            r="5.5"
+            stroke="#fff"
+            strokeLinejoin="round"
+          ></circle>
+        </svg>
+      );
+}

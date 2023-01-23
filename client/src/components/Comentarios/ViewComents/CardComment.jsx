df8d46d79263d5a976a405fe612cc2b285707f39
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-23 10:29:15 -0300

    comentarios

--- client/src/components/Comentarios/ViewComents/CardComment.jsx
+++ client/src/components/Comentarios/ViewComents/CardComment.jsx
@@ -2,19 +2,32 @@ import React from "react";
 import style from "./CardComment.module.css";
 export default function CardComment() {
   return (
-    <div className={style.layout}>
-      {" "}
-      <div className={style.divIcon}>
-        <div className={style.divAvatar}>
-          <img
-            className={style.img}
-            src={"https://cdn-icons-png.flaticon.com/512/1361/1361876.png"}
-            alt="ImgProfile"
-            width="100px"
-            height="100px"
-          />
+    <>
+      <div className={style.layout}>
+        {" "}
+        <div className={style.divIcon}>
+          <div className={style.divAvatar}>
+            <img
+              className={style.img}
+              src={"https://cdn-icons-png.flaticon.com/512/1361/1361876.png"}
+              alt="ImgProfile"
+              width="100px"
+              height="100px"
+            />
+          </div>
+        </div>
+        <div className={style.contenedor}>
+          <div className={style.flecha_left}></div>{" "}
+          <div className={style.coment}>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              e{" "}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

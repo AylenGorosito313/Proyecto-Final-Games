351720c9b68f2a1f1c49fc19869cfdea11f3015c
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-23 00:26:32 -0300

    asdasd

--- client/src/components/Comentarios/InputComent/CommentInput.jsx
+++ client/src/components/Comentarios/InputComent/CommentInput.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import style from "./CommentInput.module.css";
 export default function CommentInput() {
   const { userActual } = useSelector((state) => state.prueba);
+   const dispatch =useDispatch()
 
+   const handlerSubmit = (data)=>{
+    dispatch((data))
+   }
   return (
     <>
     <div className={style.layout}>
   
-      <form className={style.container}>
+      <form onSubmit={handlerSubmit} className={style.container}>
         <textarea className={style.input} type="textarea" />
         <button className={style.button}>Comemnt</button>
       </form>
--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -29,7 +29,8 @@ import {
   deleteProvisoryFavoriteIds,
   responseCreateBanner,
   responseDeleteeBanner,
-  getAll_Banner
+  getAll_Banner,
+  resAddComment
 } from "../reducers/prueba/pruebaSlider";
 // localhost:3001/games/filters/examinar/routes
 export const getGames = () => {
@@ -618,5 +619,31 @@ export const putUser = (inf) => {
       }catch(e){
           console.log(e);
       }
+  };
+};
+
+
+// add comment 
+export const addComments = (coment, userId, gameId ) => {
+  return async function (dispatch) {
+    try {
+      let res = await axios({
+        method: "POST",
+        data: coment ,
+        url: `http://localhost:3001/user/add/coment?userId=${userId}gameId=${gameId} `,
+      });
+      dispatch(resAddComment(res));
+    } catch (error) {
+      toast.error(error.message, {
+        position: "bottom-right",
+        duration: 4000,
+
+        style: {
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+        },
+      });
+    }
   };
 };

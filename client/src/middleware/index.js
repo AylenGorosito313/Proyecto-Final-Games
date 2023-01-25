06fdae2bc03a0cb0ba66ac44ff462964302f66ab
Daniel9822 <dhdany64@gmail.com>
2023-01-25 18:23:44 -0400

    update

--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -45,7 +45,7 @@ import {
   getByNameDb
 } from "../reducers/prueba/pruebaSlider";
 
-let url = "https://backend-pf-production.up.railway.app" // http://localhost:3001 (agregar cuando estás en rama )
+let url = "http://localhost:3001" //  https://backend-pf-production.up.railway.app (agregar cuando estás en rama )
 
 export const getGames = () => {
   return async function (dispatch) {
